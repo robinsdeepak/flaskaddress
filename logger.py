64d2c1c0b6d62ed7622d1134647fea6fc0eb7b3d
Apple <apple@mac.local>
2023-05-07 13:00:09 +0530

    improved logger file

--- logger.py
+++ logger.py
@@ -1,4 +1,34 @@
-import logging
+import os, logging, errno
+from logging import handlers
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+
+def init_logger(
+    filename=None,
+    level=logging.INFO,
+    frmt=None,
+    maxbytes=104857600,
+    backupcount=5,
+):
+    log = logging.getLogger(filename)
+    log.setLevel(level)
+
+    if frmt is None:
+        frmt = "%(asctime)s %(filename)s:%(lineno)s %(levelname)s: %(process)d: %(message)s"
+
+    formatter = logging.Formatter(frmt)
+    try:
+        handler = handlers.RotatingFileHandler(
+            filename, maxBytes=maxbytes, backupCount=backupcount
+        )
+        log.addHandler(handler)
+        handler.setFormatter(formatter)
+    except:
+        pass
+
+    return log
+
+
+logger = init_logger(
+    filename="logs.log",
+    level=logging.DEBUG,
+)
